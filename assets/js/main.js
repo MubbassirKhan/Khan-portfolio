932adee1268945f9d249c7ce657e89a558263855
sunny <119472586+crazyscriptright@users.noreply.github.com>
2026-02-27 17:42:53 +0530

    updated landingpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,10 +81,11 @@
    */
   function aosInit() {
     AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
+      duration: 800,
+      easing: 'ease-out-cubic',
       once: true,
-      mirror: false
+      mirror: false,
+      offset: 60
     });
   }
   window.addEventListener('load', aosInit);
